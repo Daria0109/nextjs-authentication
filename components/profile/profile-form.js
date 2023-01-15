fc2ad8c92458bf1b55f5bf36b733794c1b23e5ab
Darya Shnipova <dshnipova@gmail.com>
2023-01-15 01:20:13 +0100

    Add change-password functionality

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,18 +1,57 @@
 import classes from './profile-form.module.css';
+import { useState } from 'react';
+
+async function changePassword(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+        method: 'PATCH',
+        body: JSON.stringify(passwordData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const data = await response.json();
+    if(!response.ok) {
+        throw new Error(data.message || 'Something went wrong...')
+    } else {
+        return data;
+    }
+}
 
 function ProfileForm() {
-  return (
-    <form className={classes.form}>
+    const [newPassword, setNewPassword] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+
+    function handleNewPasswordChange(e) {
+        setNewPassword(e.target.value);
+    }
+
+    function handleOldPasswordChange(e) {
+        setOldPassword(e.target.value);
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const result = await changePassword({ newPassword, oldPassword });
+            console.log(result);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+    <form className={classes.form} onSubmit={handleSubmit}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' value={newPassword} onChange={handleNewPasswordChange} />
       </div>
       <div className={classes.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input type='password' id='old-password' value={oldPassword} onChange={handleOldPasswordChange} />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button type="submit">Change Password</button>
       </div>
     </form>
   );
---
+++ pages/api/user/change-password.js
@@ -0,0 +1,44 @@
+import { getSession } from 'next-auth/client';
+import connectToDataBase from '../../../lib/database';
+import { hashPassword, verifyPassword } from '../../../lib/auth';
+
+async function handler(req, res ) {
+	if (req.method !== 'PATCH') {
+		return;
+	}
+	const { oldPassword, newPassword } = req.body;
+
+	const session = await getSession({ req });
+	if (!session) {
+		res.status(401).json({ message: 'User is not authenticated!' });
+		return;
+	}
+	const userEmail = session.user.email;
+
+	const client = await connectToDataBase();
+	const usersCollection = client.db().collection('users');
+	const user = await usersCollection.findOne({ email: userEmail });
+	if (!user) {
+		res.status(422).json({ message: 'User is not found!' });
+		client.close();
+		return
+	}
+
+	const userPassword = user.password;
+	const passwordsAreEqual = await verifyPassword(oldPassword, userPassword);
+	if (!passwordsAreEqual) {
+		res.status(403).json({ message: 'Invalid password!' });
+		client.close();
+		return;
+	}
+
+	await usersCollection.updateOne(
+		{ email: userEmail },
+		{ $set: { password: await hashPassword(newPassword)} }
+	);
+
+	res.status(200).json({ message: 'Password is updated!' });
+	client.close();
+}
+
+export default handler;
